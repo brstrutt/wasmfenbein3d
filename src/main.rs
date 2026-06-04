2f873e2b37ee2e2a92f63a82722bba81c0593477
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-04 22:06:24 +0000

    Update hello world page to show a canvas element with a rendered rectangle in it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use web_sys::window;
+use wasm_bindgen::JsCast;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, window};
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -7,7 +8,20 @@ fn main() {
         .and_then(|win| win.document())
         .expect("Could not access the document");
     let body = document.body().expect("Could not access document.body");
-    let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
-    body.append_child(text_node.as_ref())
-        .expect("Failed to append text");
+    let canvas_node = document.create_element("canvas")
+        .expect("Failed to create canvas node");
+    body.append_child(canvas_node.as_ref())
+        .expect("Failed to append canvas node");
+
+    let canvas_node = canvas_node.dyn_into::<HtmlCanvasElement>()
+        .expect("Failed to convert canvas into HtmlCanvasElement");
+
+    let canvas_context = canvas_node.get_context("2d")
+        .expect("Failed to get 2D context")
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .expect("Failed to get 2D context even MORE");
+
+    canvas_context.set_fill_style_str("#f54f00");
+    canvas_context.fill_rect(10.0, 10.0, 100.0, 50.0);
 }
