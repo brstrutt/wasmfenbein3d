9b4c0f941e68120c07bc80974d350dc791457189
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-09 20:21:10 +0000

    Add log messages on startup

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,23 @@ mod screen;
 mod primitives;
 use screen::Screen;
 use rgb::RGB;
+use wasm_bindgen::JsValue;
 
 fn main() {
     console_error_panic_hook::set_once();
 
+    log("Starting up!");
     let screen = Screen::init();
+
+    log(format!("Screen ({},{})", screen.width, screen.height).as_str());
 
     for x in 0..=screen.width {
         for y in 0..=screen.height {
             screen.render(x, y, RGB {red: x, green: y, blue: (x + y) / 2});
         }
     }
+}
+
+fn log(message: &str) {
+    web_sys::console::log_1(&JsValue::from_str(message));
 }
