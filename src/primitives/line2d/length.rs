e7b278c4cbdf663057ac5b9d9fb06d637cab1fce
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-10 22:55:51 +0000

    Remove incorrect comment

--- src/primitives/line2d/length.rs
+++ src/primitives/line2d/length.rs
@@ -1,7 +1,6 @@
 use crate::primitives::{line2d::_Line2D, point2d::_Point2D};
 
 impl _Line2D {
-    // Implementation of a Line intersection point algorithm taken from https://web.archive.org/web/20060911055655/http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
     pub fn _length(&self) -> f64 {
         _Point2D::dist(&self.start, &self.end)
     }
