15469c64276c578062bb70a1e3b8fc0cd02f3c37
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-12 23:20:00 +0000

    Remove distance rounding that was causing very visible jumps in column height

--- src/render.rs
+++ src/render.rs
@@ -19,9 +19,11 @@ pub fn render(canvas: &MainCanvas, world: &RefCell<World>) {
         let wall_distance = world.dist_to_wall(&ray);
 
         if wall_distance.is_some() {
-            let distance = wall_distance.unwrap().round() as u32;
-            let height = canvas.element.height() / distance;
-            screen::render_column(canvas, x, height, &RGB {red: 30 * 100 / distance, green: 150 * 100 / distance, blue: 30 * 100 / distance});
+            let distance = wall_distance.unwrap();
+            if distance != 0.0 {
+                let height = canvas.element.height() as f64 / distance;
+                screen::render_column(canvas, x, height as u32, &RGB {red: 30, green: 125, blue: 30});
+            }
         }
     }
 }
