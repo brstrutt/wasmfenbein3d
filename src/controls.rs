3645b1fd5e67bea8402897dbcc6d6cd0e01d38a0
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-12 22:55:45 +0000

    Add forward/backward movement

--- src/controls.rs
+++ src/controls.rs
@@ -8,6 +8,8 @@ pub fn setup(world: Rc<RefCell<World>>) {
         match e.key().as_str() {
             "a" | "A" => world.borrow_mut().camera.origin.x -= 0.1,
             "d" | "D" => world.borrow_mut().camera.origin.x += 0.1,
+            "w" | "W" => world.borrow_mut().camera.origin.y += 0.1,
+            "s" | "S" => world.borrow_mut().camera.origin.y -= 0.1,
             &_ => return,
         }
     });
