1a7b744b69f4d6af716317c8c26393b6175f137c
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-12 23:16:55 +0000

    Speed up movement when shift is held down

--- src/controls.rs
+++ src/controls.rs
@@ -5,11 +5,15 @@ use crate::{web, world::World};
 
 pub fn setup(world: Rc<RefCell<World>>) {
     web::document::add_event_listener_with_callback(move |e: KeyboardEvent| {
+        let mut speed = 0.1;
+        if e.shift_key() {
+            speed = speed * 10.0;
+        }
         match e.key().as_str() {
-            "a" | "A" => world.borrow_mut().camera.origin.x -= 0.1,
-            "d" | "D" => world.borrow_mut().camera.origin.x += 0.1,
-            "w" | "W" => world.borrow_mut().camera.origin.y += 0.1,
-            "s" | "S" => world.borrow_mut().camera.origin.y -= 0.1,
+            "a" | "A" => world.borrow_mut().camera.origin.x += speed,
+            "d" | "D" => world.borrow_mut().camera.origin.x -= speed,
+            "w" | "W" => world.borrow_mut().camera.origin.y += speed,
+            "s" | "S" => world.borrow_mut().camera.origin.y -= speed,
             &_ => return,
         }
     });
