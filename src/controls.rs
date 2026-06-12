3601f91d290d3acad36d5a1c01cfaa11975a7711
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-12 23:30:27 +0000

    Use unused speed variable to fix release build

--- src/controls.rs
+++ src/controls.rs
@@ -5,11 +5,6 @@ use crate::{web, world::World};
 
 pub fn setup(world: Rc<RefCell<World>>) {
     web::document::add_event_listener_with_callback(move |e: KeyboardEvent| {
-        let mut speed = 0.1;
-        if e.shift_key() {
-            speed = speed * 10.0;
-        }
-
         let mut world = world.borrow_mut();
         match e.key().as_str() {
             "a" | "A" => world.camera.origin = world.camera.origin - world.camera.rotate(std::f32::consts::PI as f64 / 2.0).direction,
