1ac15b015bc31759fd9dbd11280bbfd334e63987
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-12 23:20:20 +0000

    Halve FOV to reduce aggressive visual warping

--- src/world/camera.rs
+++ src/world/camera.rs
@@ -11,7 +11,7 @@ impl Camera {
     }
 
     pub fn ray_for_column(&self, column: u32, screen_height_pixels: u32, screen_width_pixels: u32) -> Ray2D {
-        const FOV_DEGRESS: f64 = std::f32::consts::PI as f64 / 2.0; // 90 degrees in radians
+        const FOV_DEGRESS: f64 = std::f32::consts::PI as f64 / 4.0; // 45 degrees in radians
 
         let angle_step = FOV_DEGRESS / screen_height_pixels as f64;
         let horizontal_pixel_coord: i64 = column as i64 - (screen_width_pixels as i64 / 2);
