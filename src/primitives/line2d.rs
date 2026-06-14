650da2608677e1a3bea1f03750370201cea1906f
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-14 18:49:18 +0000

    Add easier to use constructor for Line2D objects

--- src/primitives/line2d.rs
+++ src/primitives/line2d.rs
@@ -8,3 +8,9 @@ pub struct Line2D {
     pub start: Point2D,
     pub end: Point2D,
 }
+
+impl Line2D {
+    pub fn new(x1: f64, y1: f64, x2: f64, y2: f64) -> Self {
+        Line2D{start: Point2D{ x: x1, y: y1 }, end: Point2D { x: x2, y: y2 }}
+    }
+}
