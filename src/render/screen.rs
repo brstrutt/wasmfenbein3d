45e89555f1e9ddeeff47f5d8c8aadb6495ce9918
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-20 10:08:55 +0000

    Speed up rendering by maintaining a single pixel index and incrementing, rather than recalculating the pixel index from an X and Y coord every time

--- src/render/screen.rs
+++ src/render/screen.rs
@@ -19,13 +19,6 @@ impl ScreenBuffer {
         }
     }
 
-    pub fn render(&mut self, x: &usize, y: &usize, color: &RGB) {
-        let pixel_index = ((y * self.width) + x) * 4;
-        self.pixels[pixel_index] = color.red;
-        self.pixels[pixel_index + 1] = color.green;
-        self.pixels[pixel_index + 2] = color.blue;
-    }
-
     pub fn render_column(&mut self, x: &usize, mut height: usize, color: &RGB) {
         if height > self.height {
             height = self.height;
@@ -46,14 +39,30 @@ impl ScreenBuffer {
             blue: 10,
         };
 
-        for y in 0..bottom {
-            self.render(&x, &y, &SKY_COLOR);
+        let pixel_increment = self.width * 4;
+        let start_pixel_index = x * 4;
+        let bottom_pixel_index = start_pixel_index + (bottom * pixel_increment);
+        let top_pixel_index = start_pixel_index + (top * pixel_increment);
+        let end_pixel_index = start_pixel_index + (self.height * pixel_increment);
+        let mut pixel_index = start_pixel_index;
+
+        while pixel_index < bottom_pixel_index {
+            self.pixels[pixel_index] = SKY_COLOR.red;
+            self.pixels[pixel_index + 1] = SKY_COLOR.green;
+            self.pixels[pixel_index + 2] = SKY_COLOR.blue;
+            pixel_index += pixel_increment;
         }
-        for y in bottom..top {
-            self.render(&x, &y, &color);
+        while pixel_index < top_pixel_index {
+            self.pixels[pixel_index] = color.red;
+            self.pixels[pixel_index + 1] = color.green;
+            self.pixels[pixel_index + 2] = color.blue;
+            pixel_index += pixel_increment;
         }
-        for y in top..self.height {
-            self.render(&x, &y, &FLOOR_COLOR);
+        while pixel_index < end_pixel_index {
+            self.pixels[pixel_index] = FLOOR_COLOR.red;
+            self.pixels[pixel_index + 1] = FLOOR_COLOR.green;
+            self.pixels[pixel_index + 2] = FLOOR_COLOR.blue;
+            pixel_index += pixel_increment;
         }
     }
 
