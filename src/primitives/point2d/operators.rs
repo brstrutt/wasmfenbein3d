d1d0affc4d150a513c47b280f734453c4fce780d
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-27 13:33:43 +0000

    Add +=/-= operators to point2D and allow adding reference objects

--- src/primitives/point2d/operators.rs
+++ src/primitives/point2d/operators.rs
@@ -11,11 +11,33 @@ impl ops::Add<Point2D> for Point2D {
     }
 }
 
+impl ops::Add<&Point2D> for &Point2D {
+    type Output = Point2D;
+
+    fn add(self, _rhs: &Point2D) -> Point2D {
+        Point2D{x: self.x + _rhs.x, y: self.y + _rhs.y}
+    }
+}
+
+impl ops::AddAssign for Point2D {
+    fn add_assign(&mut self, rhs: Point2D) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
 impl ops::Sub<Point2D> for Point2D {
     type Output = Point2D;
 
     fn sub(self, _rhs: Point2D) -> Point2D {
         Point2D{x: self.x - _rhs.x, y: self.y - _rhs.y}
+    }
+}
+
+impl ops::SubAssign for Point2D {
+    fn sub_assign(&mut self, rhs: Point2D) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
     }
 }
 
