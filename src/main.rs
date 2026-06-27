be45932c8ae6d93a87ed8938f87e3a210cb7807b
brstrutt <ben.strutt@hotmail.co.uk>
2026-06-27 14:15:13 +0000

    Add utility function for comparing floating point numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod utils;
 mod primitives;
 mod render;
 mod world;
---
+++ src/utils/assert_floats_equal.rs
@@ -0,0 +1,6 @@
+pub fn assert_floats_equal(left: f64, right: f64) {
+    let big = left.max(right);
+    let small = left.min(right);
+
+    assert!(big - small < 0.000_000_000_1, "{} does not equal {}", left, right);
+}
