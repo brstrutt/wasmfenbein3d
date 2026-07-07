441a0adf5818971a0f99057db1a3342c53ad2286
brstrutt <ben.strutt@hotmail.co.uk>
2026-07-07 21:28:27 +0000

    chore(dev): allow textures to be unused (for now, I should remove this later to reduce binary sizes)

--- src/render/textures/wall.rs
+++ src/render/textures/wall.rs
@@ -1,5 +1,6 @@
 use crate::render::tiling_texture::TilingTexture;
 
+#[allow(unused)]
 pub fn load_texture() -> TilingTexture {
     TilingTexture::new_from_bmp_data(include_bytes!("./wall.bmp"))
 }
