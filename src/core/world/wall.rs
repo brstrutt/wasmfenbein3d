98db555c91099356a9df5cb2a7cb4315c8f375aa
brstrutt <ben.strutt@hotmail.co.uk>
2026-07-14 21:58:09 +0000

    chore(render): add a painting to each wall (they dont render yet)

--- src/core/world/wall.rs
+++ src/core/world/wall.rs
@@ -1,18 +1,21 @@
 use std::{cell::RefCell, rc::Rc};
 
+use super::painting::Painting;
 use crate::core::{primitives::line2d::Line2D, render::tiling_texture::TilingTexture};
 
 #[derive(Clone)]
 pub struct Wall {
     pub position: Line2D,
     pub texture: Rc<RefCell<TilingTexture>>,
+    pub painting: Painting,
 }
 
 impl Wall {
-    pub fn new(line: Line2D, texture: &Rc<RefCell<TilingTexture>>) -> Self {
+    pub fn new(line: Line2D, texture: &Rc<RefCell<TilingTexture>>, painting: Painting) -> Self {
         Wall {
             position: line,
             texture: texture.clone(),
+            painting,
         }
     }
 }
