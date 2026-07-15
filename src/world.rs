a6f5864a05f8cb1f6f8413487eba638fb2563320
brstrutt <ben.strutt@hotmail.co.uk>
2026-07-15 18:09:30 +0000

    chore(world): turn all walls round so they are clockwise. Anticlockwise walls render textures mirrored

--- src/world.rs
+++ src/world.rs
@@ -17,49 +17,49 @@ pub fn load_walls(palette: &mut RgbPalette) -> Vec<Wall> {
     let mut result = Vec::<Wall>::new();
     result.append(&mut walls_from_point_path(
         &vec![
-            Point2D::new(-5.0, -3.0),
+            Point2D::new(-1.0, 5.0),
             Point2D::new(-5.0, 5.0),
-            Point2D::new(-1.0, 5.0),
+            Point2D::new(-5.0, -3.0),
         ],
         &wood_wall_texture,
         default_painting.clone(),
     ));
     result.append(&mut walls_from_point_path(
         &vec![
-            Point2D::new(-1.0, 5.0),
+            Point2D::new(8.0, 20.0),
             Point2D::new(-1.0, 20.0),
-            Point2D::new(8.0, 20.0),
+            Point2D::new(-1.0, 5.0),
         ],
         &stone_wall_texture,
         default_painting.clone(),
     ));
     result.append(&mut walls_from_point_path(
         &vec![
-            Point2D::new(8.0, 20.0),
-            Point2D::new(8.0, 23.0),
-            Point2D::new(15.0, 23.0),
-            Point2D::new(15.0, 15.0),
-            Point2D::new(8.0, 15.0),
             Point2D::new(8.0, 18.0),
+            Point2D::new(8.0, 15.0),
+            Point2D::new(15.0, 15.0),
+            Point2D::new(15.0, 23.0),
+            Point2D::new(8.0, 23.0),
+            Point2D::new(8.0, 20.0),
         ],
         &wood_wall_texture,
         default_painting.clone(),
     ));
     result.append(&mut walls_from_point_path(
         &vec![
-            Point2D::new(8.0, 18.0),
-            Point2D::new(1.0, 18.0),
             Point2D::new(1.0, 5.0),
+            Point2D::new(1.0, 18.0),
+            Point2D::new(8.0, 18.0),
         ],
         &stone_wall_texture,
         default_painting.clone(),
     ));
     result.append(&mut walls_from_point_path(
         &vec![
-            Point2D::new(1.0, 5.0),
-            Point2D::new(5.0, 5.0),
-            Point2D::new(5.0, -5.0),
             Point2D::new(-5.0, -5.0),
+            Point2D::new(5.0, -5.0),
+            Point2D::new(5.0, 5.0),
+            Point2D::new(1.0, 5.0),
         ],
         &wood_wall_texture,
         default_painting.clone(),
@@ -71,18 +71,18 @@ pub fn load_walls(palette: &mut RgbPalette) -> Vec<Wall> {
     ));
     result.append(&mut walls_from_point_path(
         &vec![
-            Point2D::new(-10.0, -5.0),
-            Point2D::new(-10.0, -7.0),
-            Point2D::new(-13.0, -7.0),
-            Point2D::new(-13.0, -1.0),
-            Point2D::new(-10.0, -1.0),
             Point2D::new(-10.0, -3.0),
+            Point2D::new(-10.0, -1.0),
+            Point2D::new(-13.0, -1.0),
+            Point2D::new(-13.0, -7.0),
+            Point2D::new(-10.0, -7.0),
+            Point2D::new(-10.0, -5.0),
         ],
         &wood_wall_texture,
         default_painting.clone(),
     ));
     result.append(&mut walls_from_point_path(
-        &vec![Point2D::new(-10.0, -3.0), Point2D::new(-5.0, -3.0)],
+        &vec![Point2D::new(-5.0, -3.0), Point2D::new(-10.0, -3.0)],
         &stone_wall_texture,
         default_painting.clone(),
     ));
