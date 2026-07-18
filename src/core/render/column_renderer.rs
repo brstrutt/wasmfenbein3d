0903aa5cf3863464934b35def6e18ee9f3800986
brstrutt <ben.strutt@hotmail.co.uk>
2026-07-18 12:30:07 +0000

    chore(render): use iterators instead of indexing into the array manually

--- src/core/render/column_renderer.rs
+++ src/core/render/column_renderer.rs
@@ -1,3 +1,5 @@
+use std::{iter::Peekable, slice::Iter};
+
 use super::{
     column_data::ColumnData, distance_to_brightness_level::distance_to_brightness_level,
     screen_buffer::ScreenBuffer,
@@ -7,7 +9,7 @@ use crate::core::world::{painting::Painting, wall::WALL_HEIGHT};
 pub struct ColumnRenderer<'a> {
     column: &'a ColumnData<'a>,
     screen: ScreenSpace<'a>,
-    wall_space: WallSpace,
+    wall_space: WallSpace<'a>,
     brightness_level: usize,
 }
 
@@ -17,15 +19,15 @@ struct ScreenSpace<'a> {
     current_pixel_index: usize,
 }
 
-struct WallSpace {
+struct WallSpace<'a> {
     y: f64,
     y_increment: f64,
-    current_painting: Option<PaintingSpace>,
-    next_painting_index: Option<usize>,
+    current_painting: Option<PaintingSpace<'a>>,
+    next_painting: Peekable<Iter<'a, &'a Painting>>,
 }
 
-struct PaintingSpace {
-    index: usize,
+struct PaintingSpace<'a> {
+    painting: &'a Painting,
     x: f64,
 }
 
@@ -49,11 +51,6 @@ impl<'a> ColumnRenderer<'a> {
         let column_bottom_pixel = screen_half_height - column_half_height;
 
         let wall_y = wall_starting_position as f64 / column.height_pixels;
-        let next_painting_index = if column.paintings.len() < 1 {
-            None
-        } else {
-            Some(0)
-        };
 
         ColumnRenderer {
             column,
@@ -66,7 +63,7 @@ impl<'a> ColumnRenderer<'a> {
                 y: wall_y,
                 y_increment: 1.0 / column.height_pixels,
                 current_painting: None,
-                next_painting_index,
+                next_painting: column.paintings.iter().peekable(),
             },
             brightness_level: distance_to_brightness_level(column.distance_from_camera),
         }
@@ -75,11 +72,12 @@ impl<'a> ColumnRenderer<'a> {
     pub fn render_next_pixel(&mut self, screen_buffer: &mut ScreenBuffer) -> bool {
         let wall = &self.column.nearest_wall_intersection.wall;
         let colour = if let Some(current_painting) = &self.wall_space.current_painting {
-            let painting = &self.column.paintings[current_painting.index];
-            let wall_space_y = self.wall_space.y - painting.top_left_corner.y;
-            painting.texture.get_texel(
-                (current_painting.x * painting.texture.width_f64() / painting.width) as isize,
-                (wall_space_y * painting.texture.height_f64() / painting.height) as isize,
+            let wall_space_y = self.wall_space.y - current_painting.painting.top_left_corner.y;
+            current_painting.painting.texture.get_texel(
+                (current_painting.x * current_painting.painting.texture.width_f64()
+                    / current_painting.painting.width) as isize,
+                (wall_space_y * current_painting.painting.texture.height_f64()
+                    / current_painting.painting.height) as isize,
             )
         } else {
             wall.texture.get_texel(
@@ -96,47 +94,34 @@ impl<'a> ColumnRenderer<'a> {
         Self::try_enter_next_painting(
             self.column.wall_x_pos,
             self.wall_space.y,
-            &self.column.paintings,
             &mut self.wall_space.current_painting,
-            &mut self.wall_space.next_painting_index,
+            &mut self.wall_space.next_painting,
         );
-        Self::try_leave_current_painting(
-            self.wall_space.y,
-            &self.column.paintings,
-            &mut self.wall_space.current_painting,
-        );
+        Self::try_leave_current_painting(self.wall_space.y, &mut self.wall_space.current_painting);
 
         self.screen.current_pixel_index < self.screen.column_top_pixel_index
     }
 
-    fn try_enter_next_painting(
+    fn try_enter_next_painting<'t>(
         wall_x: f64,
         wall_y: f64,
-        paintings_in_column: &[&Painting],
-        current_painting: &mut Option<PaintingSpace>,
-        mut next_painting_index: &mut Option<usize>,
+        current_painting: &mut Option<PaintingSpace<'t>>,
+        next_painting_iter: &mut Peekable<Iter<&'t Painting>>,
     ) {
-        if let Some(next_index) = &mut next_painting_index
-            && wall_y >= paintings_in_column[*next_index].top_left_corner.y
+        if let Some(next_painting) = &next_painting_iter.peek()
+            && wall_y >= next_painting.top_left_corner.y
         {
             *current_painting = Some(PaintingSpace {
-                index: *next_index,
-                x: wall_x - paintings_in_column[*next_index].top_left_corner.x,
+                painting: next_painting,
+                x: wall_x - next_painting.top_left_corner.x,
             });
-            *next_index += 1;
-            if *next_index >= paintings_in_column.len() {
-                *next_painting_index = None;
-            }
+            next_painting_iter.next();
         }
     }
 
-    fn try_leave_current_painting(
-        wall_y: f64,
-        paintings_in_column: &[&Painting],
-        current_painting: &mut Option<PaintingSpace>,
-    ) {
+    fn try_leave_current_painting(wall_y: f64, current_painting: &mut Option<PaintingSpace>) {
         if let Some(current) = &current_painting
-            && wall_y >= paintings_in_column[current.index].bottom_right_corner.y
+            && wall_y >= current.painting.bottom_right_corner.y
         {
             *current_painting = None;
         }
