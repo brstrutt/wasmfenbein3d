9b65386a8ee03bedd3e5c52ef421509f50bebc05
brstrutt <ben.strutt@hotmail.co.uk>
2026-07-23 21:33:55 +0000

    refactor(render): remove unecessary variables

--- src/core/render/row_renderer.rs
+++ src/core/render/row_renderer.rs
@@ -10,15 +10,10 @@ pub fn render_row(
     brightness: usize,
     screen_buffer: &mut ScreenBuffer,
 ) {
-    let rgb_pixel_increment = 1;
-    let row_length = screen_buffer.width;
-
-    let mut rgb_pixel_index = y * row_length;
-    let end_point = rgb_pixel_index + row_length;
-
     let mut x = 0;
     let mut pixel_index = y * screen_buffer.width;
-    while rgb_pixel_index < end_point {
+    let end_point = pixel_index + screen_buffer.width;
+    while pixel_index < end_point {
         if !screen_buffer.pixel_drawn(pixel_index) {
             let ray = camera.ray_for_column(x);
             let position = ray.origin + (ray.direction * dist_to_floor);
@@ -26,10 +21,9 @@ pub fn render_row(
                 .get_texel((position.x * 16.0) as isize, (position.y * 16.0) as isize)
                 .at_brightness(brightness);
 
-            screen_buffer.render_pixel(rgb_pixel_index, colour);
+            screen_buffer.render_pixel(pixel_index, colour);
         }
 
-        rgb_pixel_index += rgb_pixel_increment;
         x += 1;
         pixel_index += 1;
     }
