50feb4194f7375d6c68e30804aeb9d4002481992
brstrutt <ben.strutt@hotmail.co.uk>
2026-09-12 17:43:19 +0000

    chore(render): adjust darkness scaling to try and make it look better

--- src/core/render/rgb_brightness_lookup_table.rs
+++ src/core/render/rgb_brightness_lookup_table.rs
@@ -1,9 +1,9 @@
 use super::rgb::{RGB, WHITE};
 
-pub const BRIGHTNESS_STEPS: usize = 20;
+pub const BRIGHTNESS_STEPS: usize = 15;
 pub const BRIGHTNESS_STEPS_F64: f64 = BRIGHTNESS_STEPS as f64;
 pub const MAX_BRIGHTNESS_INDEX: usize = BRIGHTNESS_STEPS - 1;
-const DARKNESS_SCALE_FACTOR: f64 = 5.0 / BRIGHTNESS_STEPS_F64;
+const DARKNESS_SCALE_FACTOR: f64 = 1.3 / BRIGHTNESS_STEPS_F64;
 
 #[derive(Clone)]
 pub struct RgbBrightnessLookupTable {
@@ -16,7 +16,7 @@ impl RgbBrightnessLookupTable {
 
         for (index, value) in values.iter_mut().enumerate() {
             let lightness_index = BRIGHTNESS_STEPS_F64 - index as f64;
-            *value = max_value / f64::max(DARKNESS_SCALE_FACTOR * lightness_index, 1.0);
+            *value = max_value / (f64::max(lightness_index * DARKNESS_SCALE_FACTOR, 1.0).powi(2));
         }
 
         RgbBrightnessLookupTable { values }
